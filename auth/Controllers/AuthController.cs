ea538211d62a728535e189a6b06d37565d81e547
Hilson <82933261+Hilsonxhero@users.noreply.github.com>
2022-10-28 20:31:10 +0330

    init app

---
+++ auth/Controllers/AuthController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Web;
+using System.Web.Mvc;
+
+namespace auth.Controllers
+{
+    public class AuthController : Controller
+    {
+        // GET: Auth/Login
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        // POST: Auth/Login
+        [HttpPost]
+        public ActionResult Login(FormCollection collection)
+        {
+            return RedirectToAction("Index","Dashboard");
+        }
+
+        // GET: Auth/Register
+        public ActionResult Register()
+        {
+            return View();
+        }
+
+        // POST: Auth/Register
+        [HttpPost]
+        public ActionResult Register(FormCollection collection)
+        {
+            return RedirectToAction("Login");
+        }
+
+
+    }
+}
